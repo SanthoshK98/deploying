01d76ba7ae85edb56cd99c4c07c52aa5654a3c8e
unknown <santhoshkumarkoneti98@gmail.com>
2024-03-24 19:44:26 +0530

    ok

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const serverless = require('serverless-http')
+const port = 5888;
+
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+    return res.json({message:'Hello world'})
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`listening on the port ${port}`)
+})
+
+
+
+
+
+
+module.exports.handler = serverless(app)
